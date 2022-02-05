1bcdeff8e3699aece8e06a2ad9c36a9bb7c58ed7
angelman7 <76405247+angelman7@users.noreply.github.com>
2022-02-05 16:18:17 +0200

    5/2/2022 4:18 pm

---
+++ jetson/pillar.py
@@ -0,0 +1,9 @@
+class Pillar:
+    def __init__(self, name, lowers, uppers, min_area=400) -> None:
+        self.name = name
+        self.lowers = lowers
+        self.uppers = uppers
+        self.min_area = min_area
+    
+    def get_info(self):
+        pass
